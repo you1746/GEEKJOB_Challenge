e7f5a1ba0af54e68ae1f9ee81552ea060d926ef3
DESKTOP-LFMHLDA\you <ei9gm3xuysg2yww0@yahoo.co.jp>
2018-02-05 18:59:40 +0900

    18-3-5　特定のレコードの情報取得と表示２　課題

---
+++ CampChallenge_java/018.DB操作/test180305.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.sql.*;
+
+/**
+ *
+ * @author you
+ */
+public class test180305 {
+    public static void main(String[] args){
+        
+        Connection db_con = null;
+        PreparedStatement db_st = null;
+        ResultSet db_data = null;
+        int db_up;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","root","");
+            
+            db_st = db_con.prepareStatement("select * from profiles where name like '%茂%'");
+            
+            db_data = db_st.executeQuery();
+            while(db_data.next()){
+                System.out.print(" / " + db_data.getInt("profilesID"));
+                System.out.print(" / " + db_data.getString("name"));
+                System.out.print(" / " + db_data.getString("tel"));
+                System.out.print(" / " + db_data.getInt("age"));
+                System.out.println(" / " + db_data.getString("birthday"));
+            }
+            
+            db_data.close();
+            db_st.close();
+            db_con.close();
+        }catch(Exception e){
+            System.out.print(e);
+        };
+    }
+}
